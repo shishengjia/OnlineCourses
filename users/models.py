18a002aab7e535432c5ed6c851a008bef1277af7
SHI SHENGJIA <965769897@qq.com>
2017-01-01 16:40:36 +0800

    add  two models EmailVerifyCode and Banner

--- users/models.py
+++ users/models.py
@@ -1,18 +1,19 @@
 # -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
 
-
+# 用户
 class UserProfile(AbstractUser):
-    nickName = models.CharField(max_length=50,verbose_name=u"昵称",default="")
-    birthday = models.DateField(verbose_name=u"生日",null=True,blank=True)
-    gender = models.CharField(max_length=5,choices=(("male",u"男"),("female",u"女")),default="female")
-    address = models.CharField(max_length=100,default="")
-    mobile = models.CharField(max_length=11,null=True,blank=True)
-    image = models.ImageField(upload_to="image/%Y/%m",default=u"image/default.png",max_length=100)
+    nickName = models.CharField(max_length=50, verbose_name=u"昵称", default="")
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.CharField(max_length=5, choices=(("male", u"男"), ("female", u"女")), default="female")
+    address = models.CharField(max_length=100, default="")
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
 
     class Meta:
         verbose_name = "用户信息"
@@ -22,6 +23,31 @@ class UserProfile(AbstractUser):
         return self.username
 
 
+# 邮箱验证码
+class EmailVerifyCode(models.Model):
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    # 记录是何种类型
+    send_type = models.CharField(choices=(("register", u"注册"), ("forget", u"找回密码")), max_length=10)
+    # datetime.now()返回model编译时的时间
+    # datetime.now 返回model实例化时的时间
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u"邮箱验证码"
+        verbose_name_plural = verbose_name
 
 
+# 轮播图
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name=u"轮播图", max_length=100)
+    # 点击图片后的跳转地址
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    # 控制轮播图的顺序
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
