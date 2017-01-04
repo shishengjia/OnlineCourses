39907aa46dab8c50a33e7fe40970a628e7687158
SHI SHENGJIA <965769897@qq.com>
2017-01-04 21:08:44 +0800

    install the xadmin and register the models(users) to xadmin

---
+++ apps/users/adminx.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+
+import xadmin
+from users.models import EmailVerifyCode, Banner
+
+_author_ = 'shishengjia'
+_date_ = '04/01/2017 20:21'
+
+
+class EmailVerifyCodeAdmin(object):
+    # 配置xadmin后台管理验证码部分的显示格式
+    list_display = ['email', 'code', 'send_type', 'send_time']
+    # 配置xadmin后台管理验证码部分的搜索
+    search_fields = ['email', 'code', 'send_type']
+    list_filter = ['email', 'code', 'send_type', 'send_time']
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']
+
+
+xadmin.site.register(EmailVerifyCode, EmailVerifyCodeAdmin)
+xadmin.site.register(Banner, BannerAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -10,7 +10,7 @@ from django.db import models
 class UserProfile(AbstractUser):
     nickName = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
-    gender = models.CharField(max_length=5, choices=(("male", u"男"), ("female", u"女")), default="female")
+    gender = models.CharField(max_length=6, choices=(("male", u"男"), ("female", u"女")), default="female")
     address = models.CharField(max_length=100, default="")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
@@ -27,14 +27,18 @@ class EmailVerifyCode(models.Model):
     code = models.CharField(max_length=20, verbose_name=u"验证码")
     email = models.EmailField(max_length=50, verbose_name=u"邮箱")
     # 记录是何种类型
-    send_type = models.CharField(choices=(("register", u"注册"), ("forget", u"找回密码")), max_length=10)
+    send_type = models.CharField(choices=(("register", u"注册"), ("forget", u"找回密码")), max_length=10,
+                                 verbose_name=u"验证码类型")
     # datetime.now()返回model编译时的时间
     # datetime.now 返回model实例化时的时间
-    send_time = models.DateTimeField(default=datetime.now)
+    send_time = models.DateTimeField(default=datetime.now, verbose_name=u"发送时间")
 
     class Meta:
         verbose_name = u"邮箱验证码"
         verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return '{0}({1})'.format(self.code,self.email)
 
 
 class Banner(models.Model):
